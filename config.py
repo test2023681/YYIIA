235f60dfbc1cba453bdd9af26b00fdc31a0f461f
test2023681 <117702231+test2023681@users.noreply.github.com>
2023-01-22 15:17:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-TOKEN = os.environ.get("TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+TOKEN = os.environ.get("TOKEN", '5856353439:AAFdSE8ewLkLtmVpcuMDcIquEftCUNhP98w')
+APP_ID = int(os.environ.get("API_ID", '22140808'))
+API_HASH = os.environ.get("API_HASH", '72e39b383c4655d957f67d4ab94cb3aa')
 CHAT_ID = os.environ.get("CHAT_ID")
 
 
